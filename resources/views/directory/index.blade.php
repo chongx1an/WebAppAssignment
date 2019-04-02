106613fed06db785016e206e7e58bb405f98a391
Pang Chong Xian <chongxian0117@gmail.com>
2019-04-02 11:04:29 +0800

    Done Upload Photo

--- resources/views/directory/index.blade.php
+++ resources/views/directory/index.blade.php
@@ -9,7 +9,6 @@ use App\Category;
 @extends('layouts.app')
 
 @section('content')
-<<<<<<< HEAD
 <table class="table table-borderless">
     <thead>
       <td>@include('directory._filters')</td>
@@ -29,24 +28,6 @@ use App\Category;
                             <a href="#" class="card-link">Card link</a>
                             <a href="#" class="card-link">Another link</a>
                         </div>
-=======
-
-@if (count($tenants) > 0)
-    <div class = "row">
-        @foreach ($tenants as $i => $tenant)
-            <div class="column">
-                <div class="card" style="margin: 20px 20px 20px 20px">
-                    @if(Storage::disk('public')->exists('tenant/'.$tenant->id.'.jpg'))
-                    <img src="/storage/tenant/{{$tenant->id}}.jpg"
-                    width="240" alt="{{ $tenant->name }}">
-                    @endif
-                    <div class="container" style="width:100%">
-                        <h5 class="card-title"> {{ $tenant->name }} </h5>
-                        <h6 class="card-subtitle mb-2 text-muted"> {{ $tenant->floor->code."-".$tenant->zone->code."-".$tenant->lot_number }}</h6>
-                        <p class="card-text"> {{ $tenant->category->name }} </p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
->>>>>>> Done Upload Photo
                     </div>
                 @endforeach
             </div>
