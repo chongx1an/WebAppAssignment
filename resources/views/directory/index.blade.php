a036c5d8a85ef8c51dbafc8456cf10170bc00782
chongx1an <ch0ngx1an@hotmail.com>
2019-04-02 13:08:45 +0800

    Add link to show view

--- resources/views/directory/index.blade.php
+++ resources/views/directory/index.blade.php
@@ -18,16 +18,18 @@ use App\Category;
             @if (count($tenants) > 0)
             <div class="card-columns" style="margin: 20px 20px 20px 20px">
                 @foreach ($tenants as $i => $tenant)
-                    <div class="card shadow">
-                        <img class="card-img-top" src="https://www.aucklandairport.co.nz/-/media/Images/Traveller/Retail/Stores/Store-Main-Images/Adidas.ashx?mw=1300&hash=A76FE9990B9D3A83358256755B0823BDA9727D55" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title"> {{ $tenant->name }} </h5>
-                            <h6 class="card-subtitle mb-2 text-muted"> {{ $tenant->floor->code."-".$tenant->zone->code."-".$tenant->lot_number }}</h6>
-                            <p class="card-text"> {{ $tenant->category->name }} </p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
+                    <a href= "tenant/{{ $tenant->id }}" class="custom-card-link">
+                        <div class="card shadow">
+                            <img class="card-img-top" src="https://www.aucklandairport.co.nz/-/media/Images/Traveller/Retail/Stores/Store-Main-Images/Adidas.ashx?mw=1300&hash=A76FE9990B9D3A83358256755B0823BDA9727D55" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title"> {{ $tenant->name }} </h5>
+                                <h6 class="card-subtitle mb-2 text-muted"> {{ $tenant->floor->code."-".$tenant->zone->code."-".$tenant->lot_number }}</h6>
+                                <p class="card-text"> {{ $tenant->category->name }} </p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 @endforeach
             </div>
             @else
