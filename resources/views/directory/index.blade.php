3428b7d3d6e21cc812f55a06320166098d03317e
Pang Chong Xian <chongxian0117@gmail.com>
2019-04-02 11:04:29 +0800

    Done Upload Photo

--- resources/views/directory/index.blade.php
+++ resources/views/directory/index.blade.php
@@ -8,7 +8,10 @@
         @foreach ($tenants as $i => $tenant)
             <div class="column">
                 <div class="card" style="margin: 20px 20px 20px 20px">
-                    <img class="card-img-top" style="width:100%" src="https://www.aucklandairport.co.nz/-/media/Images/Traveller/Retail/Stores/Store-Main-Images/Adidas.ashx?mw=1300&hash=A76FE9990B9D3A83358256755B0823BDA9727D55" alt="Card image cap">
+                    @if(Storage::disk('public')->exists('tenant/'.$tenant->id.'.jpg'))
+                    <img src="/storage/tenant/{{$tenant->id}}.jpg"
+                    width="240" alt="{{ $tenant->name }}">
+                    @endif
                     <div class="container" style="width:100%">
                         <h5 class="card-title"> {{ $tenant->name }} </h5>
                         <h6 class="card-subtitle mb-2 text-muted"> {{ $tenant->floor->code."-".$tenant->zone->code."-".$tenant->lot_number }}</h6>
